79ac5d848279a2baf138e049061e66a620090c87
David <da380@cam.ac.uk>
2023-04-13 08:51:34 +0100

    CMake process started

---
+++ tests/test_mesh.f90
@@ -0,0 +1,13 @@
+program test_mesh
+
+  use module_constants
+  use module_spherical_model
+  use module_PREM
+  use module_DECK
+  use module_spherical_mesh  
+  use module_special_functions
+  implicit none
+
+  
+  
+end program test_mesh
